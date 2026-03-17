05489e95b04a63740e084cec6ae1e5c0925321e3
Ismael Suring <Ismael.Suring@Student.CSD509J.Net>
2026-03-17 08:55:13 -0700

    following instructions

--- Busking.java
+++ Busking.java
@@ -1,12 +1,26 @@
 public class Busking extends Music {
 
+    private String type;
+
     public Busking(User activeUser, String name, String time, String location) {
         super(activeUser, name, time, 0.0, location);
+        type = "busking";
+    }
+
+    public Busking()
+    {
+        super();
+        type = "incomplete";
+    }
+
+    public void Comment(Comment comment)
+    {
+        //Overriden because comments not allowed on busking posts
     }
     
     @Override
     public String toString()
     {
-        return "Post by: " + user + "\n" + name + "\n" + location + " --- " + time;
+        return "Post by: " + user + "\n" + name + "\n" + location + " --- " + time + "\n" + type;
     }
 }
